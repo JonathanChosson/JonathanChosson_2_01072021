e0c8a9126c57252441bd5cf585c6cef16a3f3a22
Jonathan Chosson <catstrike@gmail.com>
2021-07-02 15:38:40 +0200

    [JS] Validations entrées formulaire :passport_control: Issue#2

--- form.js
+++ form.js
@@ -9,8 +9,9 @@ let acceptCgu = document.getElementById('checkbox1');
 let newsletter = document.getElementById('checkbox2');
 
 //live listenner 
-first.addEventListener('input', function check() {
-    if(this.value.length > 2){
+//first name
+first.addEventListener('input', function() {
+    if(this.value.length > 1){
         first.parentElement.removeAttribute("data-error");
         first.parentElement.removeAttribute("data-error-visible");
     }else{
@@ -19,8 +20,9 @@ first.addEventListener('input', function check() {
     }
 });
 
-last.addEventListener('input', function check() {
-    if(this.value.length > 2){
+//last name
+last.addEventListener('input', function() {
+    if(this.value.length > 1){
         last.parentElement.removeAttribute("data-error");
         last.parentElement.removeAttribute("data-error-visible");
     }else{
@@ -29,11 +31,11 @@ last.addEventListener('input', function check() {
     }
 });
 
-email.addEventListener('input', function check() {
+//email
+email.addEventListener('input', function() {
     //Regex mail
     const emailReg = new RegExp(/^[\w\-\+]+(\.[\w\-]+)*@[\w\-]+(\.[\w\-]+)*\.[\w\-]{2,4}$/);
     const valid = emailReg.test(this.value);
-    console.log(valid);
     if(valid){
         email.parentElement.removeAttribute("data-error");
         email.parentElement.removeAttribute("data-error-visible");
@@ -41,6 +43,37 @@ email.addEventListener('input', function check() {
         email.parentElement.setAttribute("data-error", "Merci d'entrer une adresse mail valide");
         email.parentElement.setAttribute("data-error-visible", true);
     }
+});
+
+//quantity
+quantity.addEventListener('input', function(){
+    let entry = parseInt(this.value)
+    if(typeof(entry) === "number"){
+        quantity.parentElement.removeAttribute("data-error");
+        quantity.parentElement.removeAttribute("data-error-visible");
+    }else{
+        quantity.parentElement.setAttribute("data-error", "Merci d'entrer un chiffre");
+        quantity.parentElement.setAttribute("data-error-visible", true);
+    }
+})
+
+//location
+locations.forEach(location => {
+    let valid = false;
+    location.parentElement.setAttribute("data-error", "Merci de selectionné une ville");
+    location.parentElement.setAttribute("data-error-visible", true);
+    location.addEventListener('input', function() {
+        console.log(this.value);
+        if(this.value != null){
+            valid = true;
+            location.parentElement.removeAttribute("data-error");
+            location.parentElement.removeAttribute("data-error-visible");
+            console.log(valid);
+        }else{
+            valid = false;
+            console.log(valid);
+        }
+    })
 });
 
 //Submit btn function
